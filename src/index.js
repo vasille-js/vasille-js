6fd2f4a79a02747a17b3eea00b3802d218686322
Lelițac Vasile <lixcode@vivaldi.net>
2021-01-02 17:34:18 +0200

    Prepare to publish

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { Bind1, BindN }                                         from "./bind.js";
+import { Destroyable }                                          from "./interfaces/destroyable.js";
+import { IBind }                                                from "./interfaces/ibind.js";
+import { IValue }                                               from "./interfaces/ivalue.js";
+import { ArrayModel, MapModel, ObjectModel, SetModel }          from "./models.js";
+import { AppNode, BaseNode, ElementNode, ShadowNode, TextNode } from "./node.js";
+import { Rebind, Value }                                        from "./value.js";
+import { ArrayView, MapView, ObjectView, SetView }              from "./views.js";
+
+
+
+export {
+    Bind1, BindN,
+    Destroyable,
+    IBind, IValue,
+    ArrayModel, MapModel, ObjectModel, SetModel,
+    AppNode, BaseNode, ElementNode, ShadowNode, TextNode,
+    Rebind, Value,
+    ArrayView, MapView, ObjectView, SetView
+};
