cf604041124778b8011edfb4484786e53ea52e30
Lelițac Vasile <lixcode@vivaldi.net>
2021-08-04 20:27:46 +0300

    Fix seal function

--- src/interfaces/destroyable.js
+++ src/interfaces/destroyable.js
@@ -13,12 +13,15 @@ export class Destroyable {
                 let config = Object.getOwnPropertyDescriptor($, i);
 
                 if (!config || config.configurable) {
-                    Object.defineProperty($, i, {
+                    Object.defineProperty($, i, config?.set || config?.get ? {
+                        configurable: false,
+                        get: config?.get,
+                        set: config?.set,
+                        enumerable: config?.enumerable ?? false
+                    } : {
                         value: $[i],
                         configurable: false,
                         writable: config?.writable ?? false,
-                        get: config?.get,
-                        set: config?.set,
                         enumerable: config?.enumerable ?? false
                     });
                 }
