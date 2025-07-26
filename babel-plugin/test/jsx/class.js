b01648e87b7a340a0b0ae2039cce9da1baf54cca
Vasile <lixcode@vivaldi.net>
2025-07-26 09:45:34 +0300

    Hover class attribute with testes

--- babel-plugin/test/jsx/class.js
+++ babel-plugin/test/jsx/class.js
@@ -1,12 +1,64 @@
 import { compose } from "vasille-dx";
+let c = 'c';
 export const C = compose(Vasille => {
-  const a = Vasille.ref("a");
+  const a = Vasille.ref("a", "a");
+  const b = Vasille.ref(false, "b");
   Vasille.tag("div", {
     attr: {
       class: "static1 static2"
-    },
-    class: [a, {
-      aIsB: Vasille.expr(Vasille_a => Vasille_a === "b", a)
+    }
+  });
+  Vasille.tag("div", {
+    class: [a]
+  });
+  Vasille.tag("div", {
+    class: [{
+      aIsB: Vasille.expr(Vasille_a => Vasille_a === "b", [a])
     }]
+  });
+  Vasille.tag("div", {
+    class: [{
+      cIsB: c === "b"
+    }]
+  });
+  Vasille.tag("div", {
+    class: [c]
+  });
+  Vasille.tag("div", {
+    class: [{
+      hover: true,
+      "b": b,
+      ...{
+        active: true
+      }
+    }]
+  });
+  Vasille.tag("div", {
+    class: [...[a]]
+  });
+  Vasille.tag("div", {
+    attr: {
+      class: "a b"
+    }
+  });
+  Vasille.tag("div", {
+    attr: {
+      class: "a b"
+    }
+  });
+  Vasille.tag("div", {
+    attr: {
+      class: Vasille.expr(Vasille_a => `${Vasille_a} b`, [a])
+    }
+  });
+  Vasille.tag("div", {
+    attr: {
+      class: `${"a"} b`
+    }
+  });
+  Vasille.tag("div", {
+    attr: {
+      class: a
+    }
   });
 }, "VasilleDX:C");
--- babel-plugin/test/jsx/class.tsx
+++ babel-plugin/test/jsx/class.tsx
@@ -1,7 +1,21 @@
 import { compose } from "vasille-dx";
 
+let c = 'c';
+
 export const C = compose(() => {
   let a = "a";
+  let b = false;
 
-  <div class={["static1", "static2", a, a === "b" && "aIsB"]} />;
+  <div class={["static1", "static2"]} />;
+  <div class={[a]}/>;
+  <div class={[a === "b" && "aIsB"]}/>;
+  <div class={[c === "b" && "cIsB"]}/>;
+  <div class={[c]}/>;
+  <div class={[{ hover: true, ["b"]: b, ...{ active: true } }]}/>;
+  <div class={[...[a]]}/>;
+  <div class="a b"/>;
+  <div class={"a b"}/>;
+  <div class={`${a} b`}/>;
+  <div class={`${"a"} b`}/>;
+  <div class={a}/>;
 });
