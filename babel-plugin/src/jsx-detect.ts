8ed2a0fc06deb94a7c80480b000425a71bac41f3
Vasile <vas.lixcode@gmail.com>
2025-08-25 07:47:00 +0300

    test: Remove unreachable code from jsx-detect.ts #1

--- babel-plugin/src/jsx-detect.ts
+++ babel-plugin/src/jsx-detect.ts
@@ -5,21 +5,12 @@ export function exprHasJsx(node: types.Expression): boolean {
   return t.isJSXElement(node) || t.isJSXFragment(node);
 }
 
-export function statementHasJsx(statement: types.Statement): boolean {
-  if (t.isExpressionStatement(statement)) {
-    return exprHasJsx(statement.expression);
-  }
-  if (t.isBlockStatement(statement)) {
-    return bodyHasJsx(statement);
-  }
-
-  return false;
-}
-
 export function bodyHasJsx(node: types.BlockStatement | types.Expression): boolean {
   if (t.isExpression(node)) {
     return exprHasJsx(node);
   }
 
-  return node.body.some(statementHasJsx);
+  return node.body.some(statement => {
+    return t.isExpressionStatement(statement) && exprHasJsx(statement.expression);
+  });
 }
