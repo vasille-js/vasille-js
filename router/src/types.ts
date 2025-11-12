2869d3dc83d543b6b4ee29043f29cf7601039c69
Vasile <vas.lixcode@gmail.com>
2025-11-12 17:49:35 +0200

    fix: Fix router parameters parsing

--- router/src/types.ts
+++ router/src/types.ts
@@ -3,6 +3,7 @@ import { Fragment } from "vasille";
 export type NoParams = NonNullable<unknown>;
 
 export type RemoveTail<S extends string, Tail extends string> = S extends `${infer P}${Tail}` ? P : S;
+export type RemoveHead<S extends string, Head extends string> = S extends `${Head}${infer P}` ? P : S;
 
 // Remove all after '/', for `/123/rev-v3` use `/(id)/(name)` => {id: "123", name: "rev-v3"}
 export type GetRouteParameter<S extends string> = RemoveTail<S, `/${string}`>;
@@ -10,13 +11,16 @@ export type GetRouteParameter<S extends string> = RemoveTail<S, `/${string}`>;
 export type RouteParameters<Route extends string> = string extends Route
     ? NoParams
     : Route extends `${string}/*`
-      ? RouteParameters<RemoveTail<Route, "/*">> & { "*": string }
-      : Route extends `${string}*${string}`
-        ? NoParams
-        : Route extends `${string}/(${infer Rest})` | `${string}/(${infer Rest})/${string}`
-          ? (GetRouteParameter<Rest> extends never ? NoParams : { [P in GetRouteParameter<Rest>]: string }) &
-                (Rest extends `${GetRouteParameter<Rest>}${infer Next}` ? RouteParameters<Next> : unknown)
-          : NoParams;
+        ? RouteParameters<RemoveTail<Route, "/*">> & {
+        "*": string;
+    } : Route extends `${string}*${string}`
+            ? NoParams
+            : Route extends `(${infer Id})/${string}`
+                ? RouteParameters<RemoveHead<Route, `(${Id})/`>> & { [k in Id]: string }
+                : Route extends `${infer Head}/${string}`
+                    ? RouteParameters<RemoveHead<Route, `${Head}/`>>
+                    : Route extends `(${infer Id})` ? {[k in Id]: string }
+                        : Route extends `/${infer Path}` ? RouteParameters<Path> : NoParams;
 
 export type QueryParams = { [k: string]: string[] };
 
