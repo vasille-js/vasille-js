dfcf36d3696778b79b7d0465ac29d08bbae6ac18
Vasile <vas.lixcode@gmail.com>
2026-02-27 19:35:49 +0200

    fix: Fix JSX exports

--- jsx/src/index.ts
+++ jsx/src/index.ts
@@ -1,5 +1,5 @@
 export { Delay, For, Slot, Watch, Switch } from "./components.js";
 export { view, mount, model, store, type Composed } from "./compose.js";
 export { awaited } from "./library.js";
-export { ref, arrayModel, mapModel, expr, setModel, set, ensure, match, forward, backward } from "./internal.js";
+export { ref, arrayModel, mapModel, expr, setModel, set, ensure, match } from "./internal.js";
 export { setErrorHandler } from "vasille";
